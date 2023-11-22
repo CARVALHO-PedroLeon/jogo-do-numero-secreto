9aecf6cda560ac08d0d005581b0b25687d874cab
CARVALHO-PedroLeon <119459315+CARVALHO-PedroLeon@users.noreply.github.com>
2023-11-22 16:26:41 -0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ campo.innerHTML = texto;
 function exibirMensagemInicial () {
 
     exibirTextoNaTela('h1','Jogo do número secreto');
-    exibirTextoNaTela('p','Escolha um número entre 1 e 10' );
+    exibirTextoNaTela('p','Escolha um número entre 1 e 100' );
 }
 exibirMensagemInicial();
 
@@ -20,7 +20,7 @@ exibirMensagemInicial();
 
 function gerarNumeroAleatorio () {
 
-    let numeroEscolhido = parseInt(Math.random()*10+1) ;
+    let numeroEscolhido = parseInt(Math.random()*100+1) ;
     if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
          return gerarNumeroAleatorio();
 
